cae6a8dafdfc776f070f331c6f65b3140cccfd45
johirxofficial <johirxofficial@gmail.com>
2026-07-03 17:06:34 +0600

    Update Constants.java

--- app/src/main/java/com/sportslive/hq/app/utils/Constants.java
+++ app/src/main/java/com/sportslive/hq/app/utils/Constants.java
@@ -7,7 +7,7 @@ public class Constants {
      * This single URL drives the entire app (profile, categories, events).
      */
     public static final String CONFIG_JSON_URL =
-            "https://cdn.sportslivehq.com/config/app_data.json";
+            "https://raw.githubusercontent.com/johirxofficial/allrounderlive-2/refs/heads/main/app_data.json";
 
     public static final String EXTRA_EVENT_JSON = "extra_event_json";
 
